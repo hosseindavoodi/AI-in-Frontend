fc1386a506e97a4cdbd5b24c40fe9db071365e91
Hossein Davoodi <H.Davoodi@trenkwalder.com>
2024-03-16 11:27:17 +0300

    Implement Product detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-// App.tsx
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProductPage from "./Components/ProductPage";
+import ProductDetailsPage from "./Components/ProductDetailsPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ProductPage />} />
+        <Route path="/product/:productId" element={<ProductDetailsPage />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/ProductDetailsPage.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+const ProductDetailsPage: React.FC = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <h1>Details Page</h1>
+    </div>
+  );
+};
+
+export default ProductDetailsPage;
